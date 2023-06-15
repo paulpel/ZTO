ea80421143e632c47fe54cea645b3360ec3624de
Paul <pawelpelar@gmail.com>
2023-06-15 11:37:27 +0200

    Elitism plus refactor

--- abc.py
+++ abc.py
@@ -1,9 +1,11 @@
 import random
 
+
 class Item:
     def __init__(self, weight, value):
         self.weight = weight
         self.value = value
+
 
 def initialize_population(num_solutions, num_items):
     # Initialize a population of solutions with random binary values for each knapsack
@@ -14,6 +16,7 @@ def initialize_population(num_solutions, num_items):
             solution.append([random.randint(0, 1), random.randint(0, 1)])
         population.append(solution)
     return population
+
 
 def evaluate_solution(solution, items, max_weights):
     # Evaluate the total value of a solution for each knapsack and check if it exceeds the maximum weights
@@ -29,27 +32,34 @@ def evaluate_solution(solution, items, max_weights):
             total_values[j] = 0
     return total_values
 
+
 def local_search(solution, items, max_weights):
     # Perform a local search by flipping each bit in the solution for each knapsack and checking for improvement
     improved_solution = solution[:]
     for i in range(len(solution)):
         for j in range(len(max_weights)):
             improved_solution[i][j] = 1 - solution[i][j]
-            if evaluate_solution(improved_solution, items, max_weights)[j] > evaluate_solution(solution, items, max_weights)[j]:
+            if (
+                evaluate_solution(improved_solution, items, max_weights)[j]
+                > evaluate_solution(solution, items, max_weights)[j]
+            ):
                 solution = improved_solution[:]
             else:
                 improved_solution[i][j] = solution[i][j]
     return solution
 
+
 def roulette_wheel_selection(population, fitness_values):
     # Calculate the total fitness by summing all the values in fitness_values for each knapsack
     total_fitness = sum(sum(fitness) for fitness in fitness_values)
-    
+
     # Calculate the probabilities of selection for each solution
     probabilities = [sum(fitness) / total_fitness for fitness in fitness_values]
 
     # Calculate the cumulative probabilities for selection
-    cumulative_probabilities = [sum(probabilities[:i+1]) for i in range(len(probabilities))]
+    cumulative_probabilities = [
+        sum(probabilities[: i + 1]) for i in range(len(probabilities))
+    ]
 
     # Generate a random value and select a solution based on the cumulative probabilities
     random_value = random.random()
@@ -60,6 +70,7 @@ def roulette_wheel_selection(population, fitness_values):
     # If no solution is selected, return the last solution in the population
     return population[-1]
 
+
 def bee_colony(num_solutions, num_items, max_weights, num_iterations, items):
     # Initialize the population and the best solution
     population = initialize_population(num_solutions, num_items)
@@ -68,17 +79,26 @@ def bee_colony(num_solutions, num_items, max_weights, num_iterations, items):
     # Perform iterations of the bee colony algorithm
     for _ in range(num_iterations):
         # Evaluate fitness values for each solution in the population
-        fitness_values = [evaluate_solution(solution, items, max_weights) for solution in population]
+        fitness_values = [
+            evaluate_solution(solution, items, max_weights) for solution in population
+        ]
 
         # Perform local search and update the population
         for i in range(num_solutions):
             solution = population[i]
             neighbor = local_search(solution, items, max_weights)
-            if evaluate_solution(neighbor, items, max_weights)[0] > evaluate_solution(solution, items, max_weights)[0]:
+            if (
+                evaluate_solution(neighbor, items, max_weights)[0]
+                > evaluate_solution(solution, items, max_weights)[0]
+            ):
                 population[i] = neighbor
 
             # Update the best solution if a better solution is found
-            if best_solution is None or evaluate_solution(population[i], items, max_weights)[0] > evaluate_solution(best_solution, items, max_weights)[0]:
+            if (
+                best_solution is None
+                or evaluate_solution(population[i], items, max_weights)[0]
+                > evaluate_solution(best_solution, items, max_weights)[0]
+            ):
                 best_solution = population[i][:]
 
         # Perform roulette wheel selection and update the population
@@ -88,6 +108,7 @@ def bee_colony(num_solutions, num_items, max_weights, num_iterations, items):
                 population[i] = roulette_selection[:]
 
     return best_solution
+
 
 # Example usage
 weights = [2, 3, 5, 7, 9]
--- ga.py
+++ ga.py
@@ -1,36 +1,21 @@
 import numpy as np
 import random
 
-# Problem data
-jobs = [i for i in range(10)]  # 10 jobs
-machines = [i for i in range(3)]  # 3 machines
-time_matrix = np.random.randint(1, 10, size=(len(jobs), len(machines)))  # Processing times
 
-# Parameters
-population_size = 50
-mutation_rate = 0.1
-generations = 1000
-
-# Initialize population
-population = [random.sample(jobs, len(jobs)) for _ in range(population_size)]
-
-
-def fitness(individual):
-    # Calculate the total processing time of the jobs in the given order
+def fitness(individual, machines, time_matrix):
     total_time = np.zeros(len(machines))
     for job in individual:
         job_time = time_matrix[job]
         total_time[0] += job_time[0]
         for m in range(1, len(machines)):
-            total_time[m] = max(total_time[m], total_time[m-1]) + job_time[m]
-    return total_time[-1]  # Return time when last machine finishes
+            total_time[m] = max(total_time[m], total_time[m - 1]) + job_time[m]
+    return total_time[-1]
 
 
 def crossover(parent1, parent2):
-    # Order crossover
     size = len(parent1)
     start, end = sorted(random.sample(range(size), 2))
-    offspring = [None]*size
+    offspring = [None] * size
     offspring[start:end] = parent1[start:end]
     for gene in parent2:
         if gene not in offspring:
@@ -41,33 +26,58 @@ def crossover(parent1, parent2):
     return offspring
 
 
-def mutate(individual):
-    # Swap mutation
+def mutate(individual, mutation_rate):
     if random.random() < mutation_rate:
         idx1, idx2 = random.sample(range(len(individual)), 2)
         individual[idx1], individual[idx2] = individual[idx2], individual[idx1]
     return individual
 
 
-# Main loop
-for gen in range(generations):
-    # Evaluate fitness
-    scores = [fitness(ind) for ind in population]
-    # Select parents (roulette wheel selection)
-    # Select parents (roulette wheel selection)
-    total_fit = sum(scores)
-    selection_probs = np.array(scores) / total_fit
-    parent_indices = np.random.choice(range(len(population)), size=population_size, p=selection_probs)
-    parents = [population[i] for i in parent_indices]
-    # Create next generation
-    next_population = []
-    for i in range(0, population_size, 2):
-        offspring1 = crossover(parents[i], parents[i+1])
-        offspring2 = crossover(parents[i+1], parents[i])
-        next_population.append(mutate(offspring1))
-        next_population.append(mutate(offspring2))
-    population = next_population
+if __name__ == "__main__":
+    jobs = [i for i in range(10)]
+    machines = [i for i in range(3)]
+    time_matrix = np.random.randint(1, 10, size=(len(jobs), len(machines)))
 
-# Return best solution
-best_solution = min(population, key=fitness)
-print('Best solution: ', best_solution)
+    population_size = 50
+    mutation_rate = 0.1
+    generations = 1000
+    elitism_size = 5  # Top 5 individuals pass directly to the next generation
+
+    population = [random.sample(jobs, len(jobs)) for _ in range(population_size)]
+
+    for gen in range(generations):
+        scores = [fitness(ind, machines, time_matrix) for ind in population]
+
+        sorted_population = [x for _, x in sorted(zip(scores, population))]
+
+        next_population = sorted_population[
+            :elitism_size
+        ]  # Add elite individuals to next generation
+
+        scores = scores[elitism_size:]  # Remove elite individuals from scores
+        sorted_population = sorted_population[
+            elitism_size:
+        ]  # Remove elite individuals from population
+
+        inv_scores = 1.0 / np.array(scores)
+        total_inv_fit = sum(inv_scores)
+        selection_probs = inv_scores / total_inv_fit
+
+        parent_indices = np.random.choice(
+            range(len(sorted_population)),
+            size=(population_size - elitism_size) // 2 * 2,
+            p=selection_probs,
+        )
+
+        parents = [sorted_population[i] for i in parent_indices]
+
+        for i in range(0, len(parents) - 1, 2):  # Modify loop range due to elitism
+            offspring1 = crossover(parents[i], parents[i + 1])
+            offspring2 = crossover(parents[i + 1], parents[i])
+            next_population.append(mutate(offspring1, mutation_rate))
+            next_population.append(mutate(offspring2, mutation_rate))
+        population = next_population
+
+    best_solution = min(population, key=lambda x: fitness(x, machines, time_matrix))
+
+    print("Best solution: ", best_solution)
