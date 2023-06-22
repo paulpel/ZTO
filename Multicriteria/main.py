0f8a062ab976d83b5a058b51ee8422f961c000e8
Paul <pawelpelar@gmail.com>
2023-06-22 14:58:41 +0200

    HVI

--- Multicriteria/main.py
+++ Multicriteria/main.py
@@ -4,6 +4,7 @@ import math
 import matplotlib.pyplot as plt
 from itertools import combinations
 
+
 def makespan(individual, machines, time_matrix):
     total_time = np.zeros(len(machines))
     for job in individual:
@@ -78,11 +79,12 @@ def total_lateness(individual, machines, time_matrix, due_dates):
     return total_late
 
 
-def simulated_annealing(jobs, machines, time_matrix, due_dates, objective_functions, T=10000, alpha=0.995, T_min=0.01):
+def simulated_annealing(jobs, machines, time_matrix, due_dates, objective_functions, T=10000, alpha=0.995, T_min=0.03):
     current_schedule = jobs.copy()
     rd.shuffle(current_schedule)
     scores = []
 
+    count = 0
     while T > T_min:
         new_schedule = current_schedule.copy()
         i, j = rd.sample(range(len(new_schedule)), 2)
@@ -100,6 +102,11 @@ def simulated_annealing(jobs, machines, time_matrix, due_dates, objective_functi
         
         scores.append(current_score)
         T *= alpha
+        if count in [100, 200, 400, 900, 1600]:
+            plot_pareto(scores, [makespan, max_tardiness])
+            area = calc_h1(scores, pareto_dominance(scores))
+            print(count, area)
+        count += 1
 
     return current_schedule, scores
 
@@ -129,7 +136,6 @@ def plot_pareto(scores, objective_functions):
     # Plot all points
     scores = np.array(scores)
     plt.scatter(scores[:,0], scores[:,1], alpha=0.5)
-    
     # Plot Pareto front
     plt.plot(pareto_scores[:,0], pareto_scores[:,1], 'r-')
 
@@ -139,14 +145,42 @@ def plot_pareto(scores, objective_functions):
     plt.show()
 
 
+def calc_h1(scores, pareto):
+    k1 = [i[0] for i in scores]
+    k2 = [i[1] for i in scores] 
+    max_p = [max(k1), max(k2)]
+    points = []
+    for p in pareto:
+        if [p[0], p[1]] not in points:
+            points.append([p[0], p[1]])
+    x = [i[0] for i in points]
+    y = [i[1] for i in points]
+    whole_area = (max_p[0] - min(x))*(max_p[1]-min(y))
+    sorted_points = sorted(points, key=lambda x: x[0])
+
+    min_y = min(y)
+    for i in range(len(sorted_points) - 1):
+        whole_area -= (sorted_points[i][1] - min_y)*(sorted_points[i+1][0] - sorted_points[i][0])
+
+    return whole_area
+
+
 if __name__ == "__main__":
-    # Przykładowe użycie:
+    iterations = 10
+    hvis = []
     jobs = [i for i in range(10)]
     machines = [i for i in range(3)]
     time_matrix = np.random.randint(1, 10, size=(len(jobs), len(machines)))
     due_dates = np.random.randint(1, 100, size=len(jobs))
-    all_functions = [makespan, total_flowtime, max_tardiness, total_tardiness, max_lateness, total_lateness]
-    all_pairs = list(combinations(all_functions, 2))
-    for pair in all_pairs:
-        best_schedule, scores = simulated_annealing(jobs, machines, time_matrix, due_dates, pair)
-        plot_pareto(scores, pair)
+    all_pairs = False
+    for i in range(iterations):
+        if all_pairs:
+            all_functions = [makespan, total_flowtime, max_tardiness, total_tardiness, max_lateness, total_lateness]
+            all_pairs = list(combinations(all_functions, 2))
+            for pair in all_pairs:
+                best_schedule, scores = simulated_annealing(jobs, machines, time_matrix, due_dates, pair)
+                plot_pareto(scores, pair)
+        else:
+            best_schedule, scores = simulated_annealing(jobs, machines, time_matrix, due_dates, [makespan, max_tardiness])
+            hvis.append(calc_h1(scores, pareto_dominance(scores)))
+            plot_pareto(scores, [makespan, max_tardiness])
